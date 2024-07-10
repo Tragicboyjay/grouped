c91c73223aa6f61ef6154506cffd18bfc9b1b9eb
Tragicboyjay <124944418+Tragicboyjay@users.noreply.github.com>
2024-07-10 19:36:51 -0400

    created and test get messages
    by user, by group, by topic.

--- server/controller/messageController.js
+++ server/controller/messageController.js
@@ -23,6 +23,81 @@ async function sendMessage(req,res) {
     }
 }
 
+async function getMessagesByUser(req,res) {
+    const { userId } = req.params;
+
+    if (!userId) return res.status(400).json({ message: "No user id provided"});
+    
+    const query = util.promisify(db.query).bind(db);
+
+    try{
+        const existingUser = await query("SELECT 1 FROM users WHERE user_id = ?", [userId]);
+
+        if (existingUser.length < 1) return res.status(409).json({ message: "Invalid user id submited"});
+
+        const messages = await query("SELECT * FROM messages where authour_id = ?", [userId]);
+
+        return res.status(200).json({ message: "Messages fetched successfully", messages});
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+async function getMessagesByGroup(req,res) {
+    const { groupId } = req.params;
+
+    if (!groupId) return res.status(400).json({ message: "No group id provided"});
+    
+    const query = util.promisify(db.query).bind(db);
+
+    try{
+        const existingGroup = await query("SELECT 1 FROM `groups` WHERE group_id = ?", [groupId]);
+
+        if (existingGroup.length < 1) return res.status(409).json({ message: "Invalid group id submited"});
+        
+        const messages = await query("SELECT * FROM messages where group_id = ?", [groupId]);
+
+        return res.status(200).json({ message: "Messages fetched successfully", messages});
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
+async function getMessagesByTopic(req,res) {
+    const { topicId } = req.params;
+
+    if (!topicId) return res.status(400).json({ message: "No topic id provided"});
+    
+    const query = util.promisify(db.query).bind(db);
+
+    try{
+        const existingTopic = await query("SELECT 1 FROM `topics` WHERE topic_id = ?", [topicId]);
+
+        if (existingTopic.length < 1) return res.status(409).json({ message: "Invalid topic id submited"});
+        
+        const messages = await query(`
+            SELECT m.*
+            FROM messages m
+            JOIN \`groups\` g ON m.group_id = g.group_id
+            WHERE g.topic_id = ?;
+        `, [topicId]);
+        
+
+        return res.status(200).json({ message: "Messages fetched successfully", messages});
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+
 module.exports = {
-    sendMessage
+    sendMessage,
+    getMessagesByUser,
+    getMessagesByGroup,
+    getMessagesByTopic
 }
--- server/routes/messageRoute.js
+++ server/routes/messageRoute.js
@@ -4,9 +4,16 @@ const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 
 const {
-    sendMessage
+    sendMessage,
+    getMessagesByUser,
+    getMessagesByGroup,
+    getMessagesByTopic
 } = require("../controller/messageController")
 
 router.post("/send/:groupId", protect, sendMessage);
+
+router.get("/all/user/:userId", getMessagesByUser);
+router.get("/all/group/:groupId", getMessagesByGroup);
+router.get("/all/topic/:topicId", getMessagesByTopic);
 
 module.exports = router;
