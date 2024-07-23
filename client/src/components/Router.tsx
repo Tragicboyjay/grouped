5bc5f3a16126d46712cfeddc4d8c6ba022642b30
Tragicboyjay <124944418+Tragicboyjay@users.noreply.github.com>
2024-07-22 21:26:02 -0400

    created 404 Not Found Page

--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -6,34 +6,6 @@ import Profile from "../pages/Profile";
 import NotFound from "../pages/NotFound";
 import ChatGroup from "../pages/ChatGroup";
 
-// const Router = createBrowserRouter([
-//     {
-//         path: "/",
-//         element: <Home />
-
-//     },
-//     {
-//         path: "/sign_in",
-//         element: <SignIn />
-//     },
-//     {
-//         path: "/sign_up",
-//         element: <SignUp />
-//     },
-//     {
-//         path: "/profile",
-//         element: <Profile />
-//     },
-//     {
-//         path: "/chat/:groupId",
-//         element: <ChatGroup />
-//     },
-//     {
-//         path: "*",
-//         element: <NotFound />
-//     }
-// ])
-
 const Router = () => {
     return (  
         <Routes>
--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,6 +1,41 @@
+import { 
+    Flex,
+    Image,
+    Heading,
+    Button
+
+} from "@chakra-ui/react";
+import PageNotFound from "../assets/PageNotFound.svg";
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
+    const navigate = useNavigate();
+
     return (  
-        <><h1>Not Found</h1></>
+        <Flex
+            py="2rem"
+            width="100%"
+            height="100%"
+            alignItems="center"
+            justify="center"
+            direction="column"
+            gap="1rem"
+        >
+            <Image src={PageNotFound} alt="Page Not Found svg" boxSize="xs" />
+            <Heading size="lg">Uh-oh! We can’t find the page you’re looking for.</Heading>
+            <Flex
+                gap="1rem"
+            >
+                <Button
+
+                >Back</Button>
+                <Button 
+                    colorScheme="purple"
+                    onClick={() => navigate("/")}
+                >Home</Button>
+            </Flex>
+
+        </Flex>
     );
 }
  
