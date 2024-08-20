25e2bf82f5d0fe7e9229edfa536a9f1c087bd5e4
james <jamesfallouh@oultook.com>
2024-08-19 21:03:11 -0400

    Navbar and footer
    changed background color

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -13,84 +13,84 @@ import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 import { Link as RouterLink } from 'react-router-dom';
 
 const SocialButton = ({
-  children,
-  label,
-  href,
-}: {
+                        children,
+                        label,
+                        href,
+                      }: {
   children: React.ReactNode;
   label: string;
   href: string;
 }) => {
   return (
-    <chakra.button
-      bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
-      rounded={'full'}
-      w={8}
-      h={8}
-      cursor={'pointer'}
-      as={'a'}
-      href={href}
-      display={'inline-flex'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      transition={'background 0.3s ease'}
-      _hover={{
-        bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
-      }}>
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
+      <chakra.button
+          bg={useColorModeValue('whiteAlpha.100', 'blackAlpha.100')}
+          rounded={'full'}
+          w={8}
+          h={8}
+          cursor={'pointer'}
+          as={'a'}
+          href={href}
+          display={'inline-flex'}
+          alignItems={'center'}
+          justifyContent={'center'}
+          transition={'background 0.3s ease'}
+          _hover={{
+            bg: useColorModeValue('whiteAlpha.200', 'blackAlpha.200'),
+          }}>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        {children}
+      </chakra.button>
   );
 };
 
 const Footer: React.FC = () => {
   return (
-    <Box
-      bg={useColorModeValue('gray.50', 'gray.900')}
-      color={useColorModeValue('gray.700', 'gray.200')}>
-      <Container
-        as={Stack}
-        maxW={'6xl'}
-        py={4}
-        spacing={4}
-        justify={'center'}
-        align={'center'}>
-        <Heading size="md">Grouped</Heading>
-        <Stack direction={'row'} spacing={6}>
-          <RouterLink to={'/'}>Home</RouterLink>
-          <RouterLink to={'#'}>About</RouterLink>
-          <RouterLink to={'#'}>Services</RouterLink>
-          <RouterLink to={'#'}>Contact</RouterLink>
-        </Stack>
-      </Container>
-
       <Box
-        borderTopWidth={1}
-        borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.700')}>
+          bg="black"
+          color="white">
         <Container
-          as={Stack}
-          maxW={'6xl'}
-          py={4}
-          direction={{ base: 'column', md: 'row' }}
-          spacing={4}
-          justify={{ base: 'center', md: 'space-between' }}
-          align={{ base: 'center', md: 'center' }}>
-          <Text>© {new Date().getUTCFullYear()} Grouped. All rights reserved.</Text>
+            as={Stack}
+            maxW={'6xl'}
+            py={4}
+            spacing={4}
+            justify={'center'}
+            align={'center'}>
+          <Heading size="md" color="white">Grouped</Heading>
           <Stack direction={'row'} spacing={6}>
-            <SocialButton label={'Twitter'} href={'https://twitter.com'}>
-              <FaTwitter />
-            </SocialButton>
-            <SocialButton label={'YouTube'} href={'https://youtube.com'}>
-              <FaYoutube />
-            </SocialButton>
-            <SocialButton label={'Instagram'} href={'https://instagram.com'}>
-              <FaInstagram />
-            </SocialButton>
+            <RouterLink to={'/'}>Home</RouterLink>
+            <RouterLink to={'/about'}>About</RouterLink>
+            <RouterLink to={'/services'}>Services</RouterLink>
+            <RouterLink to={'/contact'}>Contact</RouterLink>
           </Stack>
         </Container>
+
+        <Box
+            borderTopWidth={1}
+            borderStyle={'solid'}
+            borderColor="gray.700">
+          <Container
+              as={Stack}
+              maxW={'6xl'}
+              py={4}
+              direction={{ base: 'column', md: 'row' }}
+              spacing={4}
+              justify={{ base: 'center', md: 'space-between' }}
+              align={{ base: 'center', md: 'center' }}>
+            <Text>© {new Date().getUTCFullYear()} Grouped. All rights reserved.</Text>
+            <Stack direction={'row'} spacing={6}>
+              <SocialButton label={'Twitter'} href={'https://twitter.com/jamesfallouh'}>
+                <FaTwitter />
+              </SocialButton>
+              <SocialButton label={'YouTube'} href={'https://youtube.com/jamesfallouh'}>
+                <FaYoutube />
+              </SocialButton>
+              <SocialButton label={'Instagram'} href={'https://instagram.com/jamesfallouh'}>
+                <FaInstagram />
+              </SocialButton>
+            </Stack>
+          </Container>
+        </Box>
       </Box>
-    </Box>
   );
 };
 
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,130 +4,126 @@ import {
   Flex,
   HStack,
   IconButton,
-  Button,// Import Button for logout
+  Button,
   useDisclosure,
-  useColorModeValue,
   Stack,
   Link as ChakraLink,
   Text,
   Heading,
+  useColorModeValue,
 } from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';// Use navigate for redirecting after logout
-import { Link as RouterLink, useNavigate } from 'react-router-dom';//Use navigate for redirecting after logout
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
 
-
-
 const NavLink = ({ children, to }: { children: React.ReactNode, to: string }) => (
-  <ChakraLink
-    
-    as={RouterLink}
-    to={to}
-    px={2}
-    py={1}
-    rounded={'md'}
-    _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}>
-    {children}
-  </ChakraLink>
+    <ChakraLink
+        as={RouterLink}
+        to={to}
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+          textDecoration: 'none',
+          bg: useColorModeValue('gray.700', 'gray.600'),
+          color: 'white',
+        }}
+        _focus={{
+          boxShadow: 'outline',
+        }}
+        color="white"
+    >
+      {children}
+    </ChakraLink>
 );
 
 const NavBar: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
+  const { user, logoutUser } = useAuth();
 
-  const navigate = useNavigate();//  Added navigate function for redirecting after logout
-
-  //const { user } = useAuth();
-  const { user, logoutUser } = useAuth(); // james: Access user data and logout function from the auth context
-  const handleLogout = () => { // james: Function to handle user logout
-    logoutUser(); // james: Log out the user
-    navigate('/sign_in'); // james: Redirect to the sign-in page after logout
+  const handleLogout = () => {
+    logoutUser();
+    navigate('/sign_in');
   };
 
-
-  const Links = !user ? [
-    { label: 'Home', path: '/' },
-    { label: 'About', path: '/about' },
-    { label: 'Contact', path: '/contact' },
-  ] : [
-    { label: 'Home', path: '/' },
-    { label: 'About', path: '/about' },
-    { label: 'Contact', path: '/contact' },
-    { label: 'Groups', path: '/groups' },
-    { label: 'Profile', path: '/profile' },
-  ];
-
+  const Links = !user
+      ? [
+        { label: 'Home', path: '/' },
+        { label: 'About', path: '/about' },
+        { label: 'Contact', path: '/contact' },
+      ]
+      : [
+        { label: 'Home', path: '/' },
+        { label: 'About', path: '/about' },
+        { label: 'Contact', path: '/contact' },
+        { label: 'Groups', path: '/groups' },
+        { label: 'Profile', path: '/profile' },
+      ];
 
   return (
-    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-      <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-        <IconButton
-          size={'md'}
-          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={'Open Menu'}
-          display={{ md: 'none' }}
-          onClick={isOpen ? onClose : onOpen}
-        />
-        <HStack spacing={8} alignItems={'center'}>
-          <Box
-            onClick={() => navigate("/")}
-            cursor="pointer"
-          ><Heading size="md">Grouped</Heading></Box>
-          <HStack
-            as={'nav'}
-            spacing={4}
-            display={{ base: 'none', md: 'flex' }}>
-            {Links.map((link) => (
-              <NavLink key={link.label} to={link.path}>{link.label}</NavLink>
-            ))}
+      <Box bg="black" px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+              size={'md'}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={'Open Menu'}
+              display={{ md: 'none' }}
+              onClick={isOpen ? onClose : onOpen}
+              color="white"
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box
+                onClick={() => navigate("/")}
+                cursor="pointer"
+            >
+              <Heading size="md" color="white">Grouped</Heading>
+            </Box>
+            <HStack
+                as={'nav'}
+                spacing={4}
+                display={{ base: 'none', md: 'flex' }}
+            >
+              {Links.map((link) => (
+                  <NavLink key={link.label} to={link.path}>{link.label}</NavLink>
+              ))}
+            </HStack>
           </HStack>
-        </HStack>
-        <Flex alignItems={'center'}>
-          {/*{ user ? <Text cursor="pointer" onClick={() => navigate('/profile')}><i className="fa-solid fa-user"></i> {user.username}</Text> :
+          <Flex alignItems={'center'}>
+            {user ? (
+                <>
+                  <Text color="white" cursor="pointer" onClick={() => navigate('/profile')}>
+                    <i className="fa-solid fa-user"></i> {user.username}
+                  </Text>
+                  <Box mx={2}></Box>
+                  <Button colorScheme="red" size="sm" onClick={handleLogout}>
+                    Logout
+                  </Button>
+                </>
+            ) : (
                 <Button
-                onClick={() => navigate("/sign_in")}
-                _hover={{color: "purple"}}
-                variant={'solid'}
-                colorScheme={'purple'}
-                size={'sm'}>
-                Log in
-            </Button>
-            }*/}
-          { user ? (
-                  <>
-                    <Text cursor="pointer" onClick={() => navigate('/profile')}><i className="fa-solid fa-user"></i> {user.username}</Text>
-                    <Box mx={2}></Box> {/* james: Add space between username and logout button */}
-                    <Button colorScheme="red" size="sm" onClick={handleLogout}>
-                      Logout
-                    </Button> {/* james: Added Logout button next to username */}
-                  </>
-              ) :
-              <Button
-                  onClick={() => navigate("/sign_in")}
-                  _hover={{color: "purple"}}
-                  variant={'solid'}
-                  colorScheme={'purple'}
-                  size={'sm'}>
-                Log in
-              </Button>
-          }
-
-
+                    onClick={() => navigate("/sign_in")}
+                    _hover={{ color: "purple" }}
+                    variant={'solid'}
+                    colorScheme={'purple'}
+                    size={'sm'}
+                >
+                  Log in
+                </Button>
+            )}
+          </Flex>
         </Flex>
-      </Flex>
 
-      {isOpen ? (
-        <Box pb={4} display={{ md: 'none' }}>
-          <Stack as={'nav'} spacing={4}>
-            {Links.map((link) => (
-              <NavLink key={link.label} to={link.path}>{link.label}</NavLink>
-            ))}
-          </Stack>
-        </Box>
-      ) : null}
-    </Box>
+        {isOpen ? (
+            <Box pb={4} display={{ md: 'none' }}>
+              <Stack as={'nav'} spacing={4}>
+                {Links.map((link) => (
+                    <NavLink key={link.label} to={link.path}>{link.label}</NavLink>
+                ))}
+              </Stack>
+            </Box>
+        ) : null}
+      </Box>
   );
 }
 
